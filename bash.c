f72870f74eeeda70a8f5c57957ced48c2873b74b
michaaalD <93281456+michaaalD@users.noreply.github.com>
2021-12-27 19:04:48 +0100

    Update bash.c

--- bash.c
+++ bash.c
@@ -16,7 +16,44 @@ cd /home/mdos | ls -t lab* | head -1
 
 cd /home/mdos | sed 's/Eeyore/Klapouchy/g' lab04_7.txt
 
-
 8.
+
+9.
+cd /home/mdos | sed 'a/JPEG/jpg/g' 
+11.
+mail -s "Diablo" name_surname@student.pwr.edu.pl << "Diablo is alive!"
+
+12.
+#!/bin/bash
+
+# skrypt sprawdzajacy okresowo, czy jest zalogowany uzytkownik o podanym loginie
+# wywolanie:  czy_jest login_uzytkownika
+
+argc=$#            #dane, by pokazac sposob tworzenia zmiennych
+                   #wokol operatora przypisania = nie moze byc zadnej spacji!!!
+
+# sprawdzamy, czy podano wymagany argument 
+if [ $argc -ne 1 ]; then  # [ ] - alternatywny sposob wywolywania polecenia test
+                     # operator != porowna argumenty traktujac je jako napisy!!!
+    echo "Wywolanie: $0 login_uzytkownika"
+    exit 1
+fi
+
+# alternatywne sposoby sprawdzenia czy podano wymagany argument
+#test $argc -ne 1 &&   echo "Wywolanie: $0 login_uzytkownika" && exit 1
+#test $argc -eq 1 || { echo "Wywolanie: $0 login_uzytkownika" && exit 1; }
+
+
+# dopoki nie ma uzytkownika usypiaj
+while test `who |grep $1 |wc -l` -eq 0; do sleep 2; done
+
+# uzytkownik pojawil sie - poinformuj
+echo
+echo -----------------
+echo $1 pracuje 
+echo -----------------
+mail -s "$1" name_surname@student.pwr.edu.pl << "Uzytkownik $1 jest zalogowany"
+
+exit 0
 
 
