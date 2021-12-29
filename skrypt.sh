6e22c7a6fbf1fc56067b45f0e095a0b8af0c032f
michaaalD <93281456+michaaalD@users.noreply.github.com>
2021-12-29 12:26:32 +0100

    Create skrypt.sh

---
+++ skrypt.sh
@@ -0,0 +1,25 @@
+#!usr/bin/bash
+
+echo Podaj sciezke do katalogu, wymiar x wymiar y 
+
+cd $1
+
+for i in *JPEG
+do
+mv "$i" `echo "$i" sed 's/JPEG/jpg/'` 
+done
+
+for i in *PNG
+do
+mv "$i" `echo "$i" sed 's/PNG/png/'` 
+done
+
+for i in *png
+do 
+mv "$i" `echo "$i" sed 's/ /_/'` 
+done
+
+for i in *jpg 
+do 
+mv "$i" `echo "$i" sed 's/ /_/'` 
+done
