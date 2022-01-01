c7c243874d20bb63394e5dceab812845c49ee263
michaaalD <93281456+michaaalD@users.noreply.github.com>
2022-01-01 18:59:28 +0100

    Update skrypt.sh

--- skrypt.sh
+++ skrypt.sh
@@ -1,73 +1,48 @@
 #!usr/bin/bash
 
-echo Podaj sciezke do katalogu, wymiar x wymiar y 
-
-cd $1
-
-for i in *JPEG
-do
-mv $i $(echo $i | sed 's/JPEG/jpg/g')
-done
-
-for i in *PNG
-do
-mv $i $(echo $i | sed 's/PNG/png/g')
-done
-
-for i in *png
-do 
-mv $i $(echo $i | sed 's/ /_/g')
-done
-
-for i in *jpg
-do 
-mv $i $(echo $i | sed 's/ /_/g')
-done
-
-mogrify -resize $2% *.png
-
-mogrify -resize $2% *.jpg
-
-
-tar -cf archiwum_plikow.tar 
-
-
-
-#!usr/bin/bash
-
-#sciezka do folderu z plikami, wybierana jako 1 argument przy wywolaniu skryptu w termnialu
-# 2 argument wywolania skryptu to procentowa wartosc zmiany wymiaru obrazkow
+#sciezka do folderu z plikami, wybierana jako $1 argument przy wywolaniu skryptu w termnialu
+# $2 argument wywolania skryptu to procentowa wartosc zmiany wymiaru obrazkow
 
 cd $1 
 
 # zmiana nazwy plikow z PNG na png
-for i in *PNG
+for i in *PNG;
 do
-    mv $i $(echo $i | sed 's/PNG/png/g');
+    mv "$i" "${i//PNG/png}";
 done
 
 # zmiana nazwy plikow z JPG na jpg
-for i in *JPEG
+for i in *JPG;
 do
-mv $i $(echo $i | sed 's/JPG/jpg/g');
+    mv "$i" "${i//JPG/jpg}";
 done
 
 # zmiana nazwy plikow png ze spacja na 'podloge'
-for i in *png
+for i in *png;
 do 
-mv $i $(echo $i | tr ' ' '_');
+mv "$i" "${i// /_}";
 done
 
 # zmiana nazwy plikow jpg ze spacja na 'podloge'
-for i in *jpg
+for i in *jpg;
 do 
-mv $i $(echo $i | tr ' ' '_');
+mv "$i" "${i// /_}";
+done
+
+# zmiana nazwy plikow jpeg ze spacja na 'podloge'
+for i in *jpeg;
+do 
+mv "$i" "${i// /_}";
 done
 
 #zmiana rozmiaru plikow podajac procentowa wartosc oryginalu
 mogrify -resize $2% *.png
 
 mogrify -resize $2% *.jpg
+
+mogrify -resize $2% *.jpeg
+
+
 
 #utworzenie archiwum i przeniesienie do katalogu 
 tar -cf archiwum_plikow.tar lab04_test/
@@ -80,13 +55,8 @@ echo Zmieniono nazwy plikow i wymiary zdjec
 # data 29.12.2021r.
 #TESTY:
 # Pierwsze testy zmiany nazwy i rozmiaru obrazkow  przeprowadzilem na plikach z poprzednich cwiczen, ze zmienionymi nazwami rozszerzen na wielkie litery. Program poprawnie zmienial nazwy plikow i rozm# iary obrazkow, co bylo widoczne przy uzyciu komendy 'display'. Nastepnie dodalem komende tworzaca archiwum. Program rowniez poprawnie towrzyl archiwum zmienionych plikow do wskazanego katalogu.
-# Pozniej testy wykonwyalem przy uzyciu plikow do pracy z programem. Dla tych plikow program rowniez poprawnie dzialal.
+# Pozniej testy wykonwyalem przy uzyciu plikow do pracy z diablo. Dla tych plikow program rowniez poprawnie dzialal.
 #WNIOSKI:
 # Program wydaje sie poprwanie zmieniac nazwy plikow i ich rozmiary.
-
-#sed 's/ /_/g
-
-
-
 
 
